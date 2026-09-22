f087b7b7060f5666d0cba49dab39e381dad53364
Bongo <hashir447079@gmail.com>
2026-09-22 08:09:57 +0500

    shifting to qt

--- README.md
+++
@@ -1,3 +0,0 @@
-# Rewrite in rust
-
-Rewrite humans in rust. Cats too cause they have seg faults due to mem.
--- src/ui/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod card;
-pub mod card_editor;
-pub mod java_editor;
-pub mod settings;
-pub mod window;
-
-pub mod downloader;
-pub mod server_editor;
-pub mod server_runner;
